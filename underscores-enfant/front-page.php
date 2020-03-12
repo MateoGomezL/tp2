d177711a5353de43cc9670e77fd8d0c7fbb0ae73
Mateo Gomez Lalonde <mateogl@videotron.ca>
2020-03-12 17:09:07 -0400

    Commencement de la page d'accueil

--- underscores-enfant/front-page.php
+++ underscores-enfant/front-page.php
@@ -25,13 +25,43 @@ get_header();
 			get_template_part( 'template-parts/content', 'page' );
 
         endwhile; // End of the loop.
+
+        echo "<h1>Nos dernières nouvelles</h1>";
+
+        // The Query
+        $args = array(
+            "category_name" => "nouvelle",
+            "posts_per_page" => 3,
+            "order" => "ASC"
+        );
+        $query1 = new WP_Query( $args );
         
-        echo "<h1>Nos dernières conférences</h1>";
+        // The Loop
+        echo '<div class="ConteneurNouvelles">';
+        while ( $query1->have_posts() ) {
+            $query1->the_post();
+            echo '<h4>' . get_the_title() . '</h4>';
+            echo the_post_thumbnail(null, "thumbnail");
+
+        }
+        echo '</div>';
+        
+        /* Restore original Post Data 
+        * NB: Because we are using new WP_Query we aren't stomping on the 
+        * original $wp_query and it does not need to be reset with 
+        * wp_reset_query(). We just need to set the post data back up with
+        * wp_reset_postdata().
+        */
+        wp_reset_postdata();
+        
+        echo "<h1>Nos derniers événements</h1>";
 
         /* The 2nd Query (without global var) */
         
         $args2 = array(
-            "category_name" => "conference"
+            "category_name" => "evenement",
+            "posts_per_page" => 3,
+            "order" => "ASC"
         );
         $query2 = new WP_Query( $args2 );
         $CatID = get_the_category($query2->post->ID);
@@ -55,36 +85,6 @@ get_header();
         }
         
         // Restore original Post Data
-        wp_reset_postdata();
-
-
-        echo "<h1>Nos dernières nouvelles</h1>";
-
-        // The Query
-        $args = array(
-            "category_name" => "nouvelle",
-            "posts_per_page" => 5,
-            "orderby" => "date",
-            "order" => "DESC"
-        );
-        $query1 = new WP_Query( $args );
-        
-        // The Loop
-        echo '<div class="ConteneurNouvelles">';
-        while ( $query1->have_posts() ) {
-            $query1->the_post();
-            echo '<h4>' . get_the_title() . '</h4>';
-            echo the_post_thumbnail(null, "thumbnail");
-
-        }
-        echo '</div>';
-        
-        /* Restore original Post Data 
-        * NB: Because we are using new WP_Query we aren't stomping on the 
-        * original $wp_query and it does not need to be reset with 
-        * wp_reset_query(). We just need to set the post data back up with
-        * wp_reset_postdata().
-        */
         wp_reset_postdata();
         
         ?>
